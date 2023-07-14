44c4259869b08abeaa7bdc4293636abb725eef56
student <student@gnu.ac.in>
2023-07-14 12:37:36 +0530

    Commit

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,19 @@
+fun main ()
+{
+    println("Hello Word")
+    var a=22
+    println("Integer Value : $a")
+    var b=1.5
+    println("Float Value : $b")
+    var c= "T"
+    println("Character Value : $c")
+    var d= "HMS"
+    println("String Value : $d")
+    var e="False"
+    println("Boolean Value : $e")
+    var f=96.36
+    println("Double Value : $f")
+    var g=338927847
+    println("Long  Value : $g")
+}
+
